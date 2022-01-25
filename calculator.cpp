b3cddfb6ac5abc5dfda7796b980a66b2110ae180
Jessie <jessieshukeireh2001@gmail.com>
2022-01-24 21:10:41 -0500

    refactored

--- calculator.cpp
+++ calculator.cpp
@@ -4,32 +4,45 @@ using namespace std;
 
 void calculateNumbers()
 {
-  cout << "What math operation would you like to perform? (Add, Subtract, Multiply, Divide)" << endl;
-  string response;
-  cin >> response;
-  int num1;
-  int num2;
-  cout << "What is the first number? ";
-  cin >> num1;
-  cout << "What is the second number? ";
-  cin >> num2;
-  int result;
-  if (response == "Add")
+  string userResponse;
+  cout << "Do you need to do some simple math?" << endl;
+  cin >> userResponse;
+  if (userResponse == "Yes")
   {
-    result = num1 + num2;
-    cout << "Result is: " << result << endl;
-  } else if (response == "Subtract")
+    cout << "What math operation would you like to perform? (Add, Subtract, Multiply, Divide)" << endl;
+    string response;
+    cin >> response;
+    int num1;
+    int num2;
+    cout << "What is the first number? ";
+    cin >> num1;
+    cout << "What is the second number? ";
+    cin >> num2;
+    int result;
+    if (response == "Add")
+    {
+      result = num1 + num2;
+      cout << "Result is: " << result << endl;
+    }
+    else if (response == "Subtract")
+    {
+      result = num1 - num2;
+      cout << "Result is: " << result << endl;
+    }
+    else if (response == "Multiply")
+    {
+      result = num1 * num2;
+      cout << "Result is: " << result << endl;
+    }
+    else
+    {
+      result = num1 / num2;
+      cout << "Result is: " << result << endl;
+    }
+  }
+  else
   {
-    result = num1 - num2;
-    cout << "Result is: " << result << endl;
-  } else if (response == "Multiply")
-  {
-    result = num1 * num2;
-    cout << "Result is: " << result << endl;
-  } else
-  {
-    result = num1 / num2;
-    cout << "Result is: " << result << endl;
+    cout << "Okay no problem, when you need me you know where to look :)" << endl;
   }
 }
 
